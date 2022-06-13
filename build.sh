ea2647141da24786a82acd1b852d9bbf6b3f0bf7
corentin-luc-artaud <corentin.luc.artaud@gmail.com>
2022-06-13 20:51:27 +0200

    build for armv6

--- build.sh
+++ build.sh
@@ -1,12 +1,24 @@
 export CGO_ENABLED=0
 
+rm -rf build/
+
 #Build linux
 export GOOS=linux
 export GOARCH=amd64
+echo $GOOS $GOARCH
 go build -o build/dnshield -ldflags "-w -s" ./cmd/dnshield/main.go
-upx --best --lzma build/dnshield
+upx --best --lzma build/dnshield &>/dev/null
+
+#BuildArm 32
+export GOARCH=arm
+echo $GOOS $GOARCH
+go build -o build/dnshield_arm32 -ldflags "-w -s" ./cmd/dnshield/main.go
+upx --best --lzma build/dnshield_arm32 &>/dev/null
+
 
 #Build windows
 export GOOS=windows
 export GOARCH=amd64
+echo $GOOS $GOARCH
 go build -o build/dnshield.exe ./cmd/dnshield/main.go
+upx --best --lzma build/dnshield.exe &>/dev/null
